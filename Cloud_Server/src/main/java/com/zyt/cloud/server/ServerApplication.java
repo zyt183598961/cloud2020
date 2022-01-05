2ae97b4d3c0d78deb4696f9cad7a02bdba613634
yantao_zhang <183598961@qq.com>
2022-01-05 10:25:10 +0800

    添加作者

--- Cloud_Server/src/main/java/com/zyt/cloud/server/ServerApplication.java
+++ Cloud_Server/src/main/java/com/zyt/cloud/server/ServerApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+
+/**
+ * @author yantao_zhang
+ */
 @SpringBootApplication
 //开启eureka中心
 @EnableEurekaServer
