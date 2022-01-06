4b5c09a2a1460b355e55689570df11c5f57397f8
yantao_zhang <183598961@qq.com>
2022-01-06 15:12:40 +0800

    @FeignClient注解 添加降级工厂方法

--- Cloud_Consumer/src/main/java/com/zyt/consumer/servie/SignService.java
+++ Cloud_Consumer/src/main/java/com/zyt/consumer/servie/SignService.java
@@ -1,14 +1,16 @@
 package com.zyt.consumer.servie;
 
+import com.netflix.ribbon.proxy.annotation.Hystrix;
 import com.zyt.common.dto.SignDTO;
 import com.zyt.common.vo.R;
+import com.zyt.consumer.fallback.SignFallBack;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "xph-provider")
+@FeignClient(value = "xph-provider", fallbackFactory = SignFallBack.class)
 public interface SignService {
 
     //签到
