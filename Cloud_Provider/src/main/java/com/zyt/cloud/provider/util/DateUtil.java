81cdd21297b06addd0f668ccb7762ebe06600f92
yantao_Zhang <183598961@qq.com>
2022-03-26 01:03:49 +0800

    单例模式获取当前时间

---
+++ Cloud_Provider/src/main/java/com/zyt/cloud/provider/util/DateUtil.java
@@ -0,0 +1,15 @@
+package com.zyt.cloud.provider.util;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateUtil {
+
+    private static SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+
+    public static SimpleDateFormat getDate(){
+        return new SimpleDateFormat();
+    }
+
+}
